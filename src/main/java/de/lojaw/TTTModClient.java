ccd25d7ff723847c612c04175c03d2bdb54dd9a5
BuildTools <unconfigured@null.spigotmc.org>
2023-07-17 12:27:15 +0200

    Initial commit

---
+++ src/main/java/de/lojaw/TTTModClient.java
@@ -0,0 +1,10 @@
+package de.lojaw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TTTModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
